910020e7938df9f264ac59791721aca62f6a7ca3
Fauzian17 <127533099+Fauzian17@users.noreply.github.com>
2024-05-22 21:51:19 +0800

    commit-3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 // ini javascripts
+
+let switchSegitiga = false;
+
+function hideOrShow() {
+  if (switchSegitiga == false) {
+    switchSegitiga = true;
+    document.getElementsByClassName("").style.display = "none";
+    document.getElementsByClassName("").style.display = "block";
+  } else {
+    switchSegitiga = false;
+    document.getElementsByClassName("").style.display = "block";
+    document.getElementsByClassName("").style.display = "none";
+  }
+}
+
+function validation() {}
